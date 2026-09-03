213c2e9f2a3a0dd895c23452f26c0ade6bdd1372
ElvinPai <135930426+ElvinPai@users.noreply.github.com>
2026-09-03 15:31:34 -0400

    Add files via upload

---
+++ backend/database/migrations/2026_09_02_180209_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('cliente_id')
+                ->constrained('clientes')
+                ->onDelete('cascade');
+
+            $table->string('nombre_cuenta');
+            $table->string('numero_cuenta')->unique();
+            $table->decimal('balance', 10, 2)->default(0);
+            $table->string('tipo_cuenta')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
