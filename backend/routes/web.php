5cba97cbbeb89cc36af2ae0f31895181ba4b18e6
ElvinPai <135930426+ElvinPai@users.noreply.github.com>
2026-09-03 15:55:23 -0400

    Agregando rutas de clientes y cuentas

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\AccountController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('farmixpro');
 });
+
+Route::resource('clientes', ClienteController::class);
+
+Route::resource('accounts', AccountController::class);
